c3e484284141d8c6563945959bfe13d67217c7b0
carabedo <carabedo@gmail.com>
2022-08-10 12:33:29 +0000

    first3

--- primer_proyecto/settings.py
+++ primer_proyecto/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^fz9mdv=cdr&^tq-9t2l7fi$1fcaivxz3ryg7elws!z5_ax7!_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
